e3be9119a9a369702210447b651b2b15eeec62f7
youtheresun <1099807917@qq.com>
2020-04-28 23:45:15 +0800

    地图功能

--- pages/map/map.js
+++ pages/map/map.js
@@ -4,41 +4,57 @@ Page({
         latitude: 23.099994,
         markers:[{
             id: 0,
-            iconPath: "address.png",
+            iconPath: "my_address.png",
             latitude: 23.099994,
             longitude: 113.324520,
-            width: 50,
-            height: 50
+            width: 20,
+            height: 20
+
         }],
-        circles:{
-            latitude:121.54409,
-            longitude:31.22114,
-            radius:10
-        },
-        locationInfo:""
+        nearbyList:[],
+        locationInfo:"",
+        search_radius:10000,
+        search_keyword:"",
+        nearbyListPoints:[]
+
     },
     onLoad: function(){
         var that = this;
         wx.getLocation({
             type: "wgs84",
             success: function(res){
-                console.log(res)
-                var latitude = res.latitude;
-                var longitude = res.longitude;
-                //console.log(res.latitude);
                 that.setData({
-                    circles:that.data.circles,
                     latitude: res.latitude,
                     longitude: res.longitude,
                     markers:[{
                         latitude: res.latitude,
-                        longitude: res.longitude
+                        longitude: res.longitude,
+                        iconPath: "my_address.png",
+                        width: 20,
+                        height: 20
                     }]
                 })
                 that.getAddressInfo(res.latitude,res.longitude)
 
             }
         })
+    },
+    getCurLocation(e){
+        console.log(e.detail)
+        let location=e.detail;
+        this.data.markers[0]={
+            latitude: location.latitude,
+            longitude:location.longitude,
+            iconPath: "my_address.png",
+            width: 20,
+            height: 20
+        }
+        this.setData({
+            latitude:location.latitude,
+            longitude:location.longitude,
+            markers:this.data.markers
+        })
+        this.getAddressInfo(location.latitude,location.longitude)
     },
     getAddressInfo(latitude,longitude){
         var that=this;
@@ -54,11 +70,86 @@ Page({
         })
 
     },
-    getCurLocation(e){
-        console.log(e.detail)
-        let location=e.detail;
-        this.getAddressInfo(location.latitude,location.longitude)
 
+    searchNearby(){
+        let that =this;
+        if(!this.data.search_keyword){
+            this.setData({
+                modalName:"Modal",
+                modal_content:"先输入搜索内容"
+            })
+            return;
+        }
+        wx.serviceMarket.invokeService({
+            service:"wxc1c68623b7bdea7b",
+            api:"poiSearch",
+            data:{
+                keyword:that.data.search_keyword,
+                boundary:"nearby("+that.data.latitude+","+that.data.longitude+","+that.data.search_radius+")"
+            }
+        }).then(res=>{
+            console.log(res)
+            let arr=res.data.data
+            let arr1=[];
+            for (let i=0;i<arr.length;i++){
+                let item={
+                    iconPath: "other_address.png",
+                    width: 20,
+                    height: 20,
+                };
+                item.id=i
+                item.longitude=arr[i].location.lng;
+                item.latitude=arr[i].location.lat;
+                arr1.push(item)
+            }
+            console.log(arr1)
+            this.setData({
+                nearbyList:arr,
+                markers:this.data.markers.concat(arr1)
+            })
+        }).catch(error=>{
+            console.log(error)
+        })
+
+
+    },
+    hideModal(){
+        this.setData({
+            modalName:"",
+        })
+    },
+    setKeyword(e){
+        console.log(e)
+        let keyword=e.detail.value;
+        this.setData({
+            search_keyword:keyword
+        })
+    },
+    goThere(e){
+        //获取目的位置坐标
+      let localtion=  e.currentTarget.dataset.location;
+      console.log(localtion)
+      let name= e.currentTarget.dataset.title
+        let plugin = requirePlugin('routePlan');
+        let key="NLPBZ-56F3I-P6EG7-5KBKJ-RHFC2-Z5FXA"
+        let referer = 'USUN';   //调用插件的app的名称
+        let startPoint=JSON.stringify({
+            'name': this.data.locationInfo,
+            'latitude':this.data.latitude,
+            'longitude': this.data.longitude
+        })//起点
+        console.log(startPoint)
+        let endPoint = JSON.stringify({  //终点
+            'name': name,
+            'latitude':localtion.lat,
+            'longitude': localtion.lng
+        });
+        wx.navigateTo({
+            url: 'plugin://routePlan/index?navigation=1&key=' + key + '&referer=' + referer + '&endPoint=' + endPoint+'&startPoint='+startPoint
+        });
+    },
+    relocate(){
+     this.onLoad();
 
     },
     onReady: function(){
