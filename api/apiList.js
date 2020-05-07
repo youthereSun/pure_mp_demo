7a62ce16a893d4fd463596a337bd4edde55ac419
youtheresun <1099807917@qq.com>
2020-05-07 17:36:45 +0800

    接口获取

--- api/apiList.js
+++ api/apiList.js
@@ -2,7 +2,8 @@
 module.exports = {
     // 登录
     LOGIN: '/login', //微信登录
-    GETLIST:'/getList'//获取列表
-
+    GETLIST:'/getList',//获取列表
+    GETSWIPERARR:'/getSwiperArr',
+    GETWATERFALL:'/getWaterFall'//获取瀑布流数据
 
 };
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,7 @@
 import api from '../../api/api'
-import {LOGIN} from '../../api/apiList'
+import {LOGIN, GETSWIPERARR, GETWATERFALL} from '../../api/apiList'
 
-Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {},
+Page({
 
     /**
      * 组件的初始数据
@@ -13,75 +9,8 @@ Component({
     data: {
         DotStyle: true,
         cardCur: 0,
-        swiperArr: [{
-            id: "1084",
-            photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486008&di=68514e7684d0753c5100994fae6456cb&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195613_xuEFP.thumb.700_0.jpeg",
-            title: "虽然你我会下落不明",
-        }, {
-            id: "1084",
-            photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
-            title: "你知道我曾为你动过情"
-        }, {
-            id: "1084",
-            photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173486004&di=3ef63a12a8372b81bfe97b81adee093d&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201612%2F07%2F20161207195604_jNSGc.thumb.700_0.jpeg",
-            title: "我欲与君相知，长命无绝衰。山无陵，江水为竭。东雷震震，夏雨雪。天地合，乃敢与君绝"
-        }, {
-            id: "1084",
-            photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1576173620777&di=54b7ee5f59c30503096ccabbe0ed9a8e&imgtype=0&src=http%3A%2F%2Fp.store.itangyuan.com%2Fp%2Fchapter%2Fattachment%2Feg-VEBjtEA%2FEgfWEgMVEgjTe_-W4BEU40u3KhifeUL1HtDvHhsVgU9hK6LqG1opiMO.jpg",
-            title: "晓看天色，暮看云，行也思君，坐也思君",
-        }],
-        dataSet: [
-            {
-                id: '5b61575a4256350d332d03a1',
-                title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,',
-                content:
-                    'Lorem ipsum dolor sit amet, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                time: 1533106010,
-                user: {
-                    avatar: 'https://cdn.ifanr.cn/ifanr/default_avatar.png',
-                    userId: 123123123,
-                    username: 'Lorem ipsum dolor sit am'
-                },
-                likedCount: 122,
-                liked: true,
-                images: [
-                    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1617497643,3423931853&fm=26&gp=0.jpg',
-                    'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1587974994276&di=195c7df1ef23b8fd8878a48644a0a245&imgtype=0&src=http%3A%2F%2F00imgmini.eastday.com%2Fmobile%2F20180824%2F20180824153817_f6bd6753f29acbba961d7d68b3ef3869_1.jpeg'
-                ]
-            },
-            {
-                id: '123123123',
-                content:
-                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                time: 1533106010,
-                user: {avatar: 'https://cdn.ifanr.cn/ifanr/default_avatar.png', userId: 123123123, username: '知晓妹'},
-                likedCount: 0,
-                liked: true
-            },
-            {
-                id: '5b61575a4256weqwe350d332d03a1',
-                content:
-                    'Lorem ipsum dolor sit amet,  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                time: 1533106010,
-                user: {avatar: 'https://cdn.ifanr.cn/ifanr/default_avatar.png', userId: 123123123, username: '知晓妹'},
-                likedCount: 0
-            },
-            {
-                id: '5b61575a42dasda56350d332d03a1',
-                content: '爱范儿，让未来触手可及。',
-                time: 1533106010,
-                user: {avatar: 'https://cdn.ifanr.cn/ifanr/default_avatar.png', userId: 123123123, username: '知晓妹'},
-                likedCount: 0
-            },
-            {
-                id: '5b61575weweqa4256350d332d03a1',
-                content: '爱范儿，让未来触手可及。',
-                time: 1533106010,
-                user: {avatar: 'https://cdn.ifanr.cn/ifanr/default_avatar.png', userId: 123123123, username: '知晓妹'},
-                likedCount: 0,
-                images: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1617497643,3423931853&fm=26&gp=0.jpg']
-            }
-        ],
+        swiperArr: [],
+        dataSet: [],
         brick_option: {
             showFullContent: true,
             // backgroundColor:"#16A085",
@@ -97,74 +26,86 @@ Component({
         }
 
     },
+    onLoad() {
+        let that = this;
+        const p1 = api.post(GETSWIPERARR)
+        const p2 = api.post(GETWATERFALL)
+        wx.showLoading()
+        Promise.all([p1, p2]).then(res=>{
+
+            let list1=res[0].swiperArr;
+            let list2=res[1].dataSet;
+            that.setData({
+                swiperArr:list1,
+                dataSet:list2
+            })
+            wx.hideLoading()
+        })
+
+    },
 
 
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-        // cardSwiper
-        cardSwiper(e) {
-            this.setData({
-                cardCur: e.detail.current
-            })
-        }
-        ,
-        getCurrent(e) {
-            let data = e.currentTarget.dataset.index;
-            let modal = e.currentTarget.dataset.modal;
-            console.log(data)
-            this.setData({
-                modalName: modal,
-                modal_content: "当前索引：" + data
-            })
-        }
-        ,
-        hideModal() {
-            this.setData({
-                modalName: null
-            })
-        }
-        ,
-        // 改变卡片展开状态事件的回调
-        handleExpand: function (event) {
-            console.log(event.detail)
-            console.log('expand call back')
-        },
+    // cardSwiper
+    cardSwiper(e) {
+        this.setData({
+            cardCur: e.detail.current
+        })
+    }
+    ,
+    getCurrent(e) {
+        let data = e.currentTarget.dataset.index;
+        let modal = e.currentTarget.dataset.modal;
+        console.log(data)
+        this.setData({
+            modalName: modal,
+            modal_content: "当前索引：" + data
+        })
+    }
+    ,
+    hideModal() {
+        this.setData({
+            modalName: null
+        })
+    }
+    ,
+    // 改变卡片展开状态事件的回调
+    handleExpand: function (event) {
+        console.log(event.detail)
+        console.log('expand call back')
+    },
 
-        // 点击卡片
-        tapCard: function (event) {
-            console.log(event.detail)
-            console.log('tap card!')
-            wx.navigateTo({
-                url: "/pages/item_detail/item_detail?item_id="+event.detail.card_id
-            })
-        },
+    // 点击卡片
+    tapCard: function (event) {
+        console.log(event.detail)
+        console.log('tap card!')
+        wx.navigateTo({
+            url: "/pages/item_detail/item_detail?item_id=" + event.detail.card_id
+        })
+    },
 
-        // 点赞
-        handleLike: function (event) {
-            console.log(event.detail)
-            console.log('like!')
-        },
+    // 点赞
+    handleLike: function (event) {
+        console.log(event.detail)
+        console.log('like!')
+    },
 
-        // 点击用户头像区域
-        handleUserEvent: function (event) {
-            console.log(event.detail)
-            console.log('user!')
-        },
-        onReachBottom: function () {
-            console.log('reach bottom')
-        },
-        sendRequest(){
-            let options={
-                username:"admin",
-                password:"admin"
-            };
-            api.post(LOGIN,options).then(res=>{
-                wx.setStorageSync("token",res.token)
-            }).catch(error=>{
-                console.log(error)
-            })
-        }
+    // 点击用户头像区域
+    handleUserEvent: function (event) {
+        console.log(event.detail)
+        console.log('user!')
+    },
+    onReachBottom: function () {
+        console.log('reach bottom')
+    },
+    sendRequest() {
+        let options = {
+            username: "admin",
+            password: "admin"
+        };
+        api.post(LOGIN, options).then(res => {
+            wx.setStorageSync("token", res.token)
+        }).catch(error => {
+            console.log(error)
+        })
     }
 })
